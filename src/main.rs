ef7046e2047d1c08ffe7c66af0028136d00724f4
gobtronic <1554705+gobtronic@users.noreply.github.com>
2021-02-12 09:48:42 +0100

    Cargo.lock + black pages cut handling

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ fn process_image() {
 
 fn bbox(img: &GrayImage, white: bool, tolerance: u8) -> Rect {
     let dim = img.dimensions();
-    println!("{:?}", dim);
     let mut bbox = Rect {
         x: 0,
         y: 0,
@@ -28,65 +27,60 @@ fn bbox(img: &GrayImage, white: bool, tolerance: u8) -> Rect {
 
     let mut i = Point::new(0, 0);
     for pi in img.pixels() {
-        match white {
-            true => {
-                if pi.0[0] < 255 - tolerance {
-                    if bbox.x != 0 && i.x < bbox.x {
-                        bbox = Rect {
-                            x: i.x,
-                            y: bbox.y,
-                            width: bbox.x - i.x + bbox.width,
-                            height: bbox.height,
-                        };
-                    } else if bbox.x == 0 {
-                        bbox = Rect {
-                            x: i.x,
-                            y: bbox.y,
-                            width: bbox.width,
-                            height: bbox.height,
-                        };
-                    }
+        if white && pi.0[0] < 255 - tolerance || !white && pi.0[0] > 0 + tolerance {
+            if bbox.x != 0 && i.x < bbox.x {
+                bbox = Rect {
+                    x: i.x,
+                    y: bbox.y,
+                    width: bbox.x - i.x + bbox.width,
+                    height: bbox.height,
+                };
+            } else if bbox.x == 0 {
+                bbox = Rect {
+                    x: i.x,
+                    y: bbox.y,
+                    width: bbox.width,
+                    height: bbox.height,
+                };
+            }
 
-                    if bbox.x > 0 {
-                        if bbox.x + bbox.width < i.x {
-                            bbox = Rect {
-                                x: bbox.x,
-                                y: bbox.y,
-                                width: i.x - bbox.x,
-                                height: bbox.height,
-                            };
-                        }
-                    }
-
-                    if bbox.y != 0 && i.y < bbox.y {
-                        bbox = Rect {
-                            x: bbox.x,
-                            y: i.y,
-                            width: bbox.width,
-                            height: bbox.y - i.y + bbox.height,
-                        };
-                    } else if bbox.y == 0 {
-                        bbox = Rect {
-                            x: bbox.x,
-                            y: i.y,
-                            width: bbox.width,
-                            height: bbox.height,
-                        };
-                    }
-
-                    if bbox.y > 0 {
-                        if bbox.y + bbox.height < i.y {
-                            bbox = Rect {
-                                x: bbox.x,
-                                y: bbox.y,
-                                width: bbox.width,
-                                height: i.y - bbox.y,
-                            };
-                        }
-                    }
+            if bbox.x > 0 {
+                if bbox.x + bbox.width < i.x {
+                    bbox = Rect {
+                        x: bbox.x,
+                        y: bbox.y,
+                        width: i.x - bbox.x,
+                        height: bbox.height,
+                    };
                 }
             }
-            false => if pi.0[0] > 0 {},
+
+            if bbox.y != 0 && i.y < bbox.y {
+                bbox = Rect {
+                    x: bbox.x,
+                    y: i.y,
+                    width: bbox.width,
+                    height: bbox.y - i.y + bbox.height,
+                };
+            } else if bbox.y == 0 {
+                bbox = Rect {
+                    x: bbox.x,
+                    y: i.y,
+                    width: bbox.width,
+                    height: bbox.height,
+                };
+            }
+
+            if bbox.y > 0 {
+                if bbox.y + bbox.height < i.y {
+                    bbox = Rect {
+                        x: bbox.x,
+                        y: bbox.y,
+                        width: bbox.width,
+                        height: i.y - bbox.y,
+                    };
+                }
+            }
         }
 
         if i.x + 2 > dim.0 {
